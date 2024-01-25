c38d1036ff6ad4d868612f4c7c134d50b0f9e509
Hafizh Adi Prasetya <hafizh.adi@bukalapak.com>
2024-01-25 11:46:17 +0900

    debug verprint

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 # Print but only if currently above specified verbosity level
 def verPrint(verbose_status, verbose_threshold, *args):
     if verbose_status >= verbose_threshold:
+        print(args)
         print(' '.join([str(x) for x in args]))
