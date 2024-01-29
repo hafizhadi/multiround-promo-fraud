771aedcf16ec2818d3c19b8b44afee711c2f31af
Hafizh Adi Prasetya <hafizh.adi@bukalapak.com>
2024-01-29 17:49:02 +0900

    print

--- src/experiment_multiround/supervised_multi.py
+++ src/experiment_multiround/supervised_multi.py
@@ -227,6 +227,9 @@ class MultiroundExperiment(object):
             del new_edges['src'], new_edges['dst']
 
             self.dset['graph'].add_nodes(len(new_nodes), new_nodes)
+
+            print(edge_dst, edge_src)
+            print("new", new_edges)
             self.dset['graph'].add_edges(edge_src, edge_dst, new_edges)
 
             # TODO: Update node and ground truth masks
