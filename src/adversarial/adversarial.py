a368665e4c30fb60256112d0a341d5a51de54438
Hafizh Adi Prasetya <hafizh.adi@bukalapak.com>
2024-01-31 16:52:16 +0900

    template

--- src/adversarial/adversarial.py
+++ src/adversarial/adversarial.py
@@ -3,5 +3,5 @@ class BaseAdversary():
     def __init__(self):
         pass
 
-    def generate(self, graph, n_instances=1):
+    def generate(self, graph, n_instances=1, return_seed=False):
         return 0
