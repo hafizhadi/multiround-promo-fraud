09a46461b3feefb45d3030327d5da0d7446479a1
Hafizh Adi Prasetya <hafizh.adi@bukalapak.com>
2024-02-16 16:00:56 +0900

    print labels

--- src/utils_func.py
+++ src/utils_func.py
@@ -39,7 +39,7 @@ def eval_and_print(verbose_level, labels, preds, probs, msg):
     auc = roc_auc_score(labels, probs.detach().numpy()) if torch.unique(labels).shape[0] > 1 else -1
 
     tn, fp, fn, tp = confusion_matrix(labels, preds).ravel()
-    
+    print(labels)
     verPrint(verbose_level, 1, f'{msg}: REC {rec*100:.2f} PRE {prec*100:.2f} MF1 {f1*100:.2f} AUC {auc*100:.2f} TP {tp} FP {fp} TN {tn} FN {fn} | {dict(Counter(labels.tolist()))}')
 
     return (rec, prec, f1, auc)
